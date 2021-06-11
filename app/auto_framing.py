5ab9590e5e5d998a4adffa8ea62ab249266c45d1
samuelgomes <samuelluz.g@gmail.com>
2021-06-11 20:50:58 -0300

    two images in a frame

--- app/auto_framing.py
+++ app/auto_framing.py
@@ -2,19 +2,23 @@ import cv2
 import numpy as np
 import os
 
-def detectar_enquadramento(thresh):
-    mold_area = thresh.shape[0]*thresh.shape[1]
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+def detectar_enquadramento(mold):
+    mold_thresh = (255-mold[:,:,3])
+    mold_png = cv2.merge((mold_thresh, mold_thresh, mold_thresh, mold_thresh))
+    mold_area = mold_thresh.shape[0]*mold_thresh.shape[1]
+    contours, hierarchy = cv2.findContours(mold_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    molds_bboxs = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if mold_area*0.9>area>mold_area*0.1:
             x,y,w,h = cv2.boundingRect(cnt)
+            if w == 0 or h == 0:
+                raise Exception('ERRO: Problema ao detectar enquadramento da moldura')
+            molds_bboxs.append([x,y,w,h])
             # cv2.rectangle(mold,(x,y),(x+w,y+h),(0,0,255),2)
-    if w == 0 or h == 0:
-        raise Exception('ERRO: Problema ao detectar enquadramento da moldura')
-    return x,y,w,h
+    return molds_bboxs, mold_png
 
-def correcao_tamanho(img, w,h):
+def correcao_tamanho(img, w, h):
     new_img_w = int(h*(img.shape[1]/img.shape[0]))
     if new_img_w<w:
         # iguala a largura e ajusta a altura
@@ -36,19 +40,16 @@ def correcao_tamanho(img, w,h):
     img = cv2.resize(img,(w,h))
     return img
 
-def put_frame(img, mold):
-    mold_h, mold_w, _ = mold.shape
-    thresh = (255-mold[:,:,3])
-    png = cv2.merge((thresh, thresh, thresh, thresh))
-    x,y,w,h = detectar_enquadramento(thresh)
-    # adiciona um canal a img
-    img = np.dstack([img, np.ones((img.shape[0], img.shape[1]), dtype="uint8") * 255])
-    img = correcao_tamanho(img,w,h)
-    ovr = np.zeros((mold_h,mold_w,4), dtype="uint8")
+def put_frame(img, mold, mold_bbox, mold_png):
+    mold_h, mold_w, _ = mold_png.shape
+    bbox_x, bbox_y, bbox_w, bbox_h = mold_bbox
+    img = correcao_tamanho(img, bbox_w, bbox_h)
+    # ovr = np.zeros((mold_h,mold_w, 4), dtype="uint8")
+    ovr = mold
 
-    ovr[y:y+h,x:x+w,:] = img
-    dst = cv2.bitwise_and(mold, (255-png))
-    dst2 = cv2.bitwise_and(ovr, png)
+    ovr[bbox_y:bbox_y+bbox_h, bbox_x:bbox_x+bbox_w,:] = img
+    dst = cv2.bitwise_and(mold, (255-mold_png))
+    dst2 = cv2.bitwise_and(ovr, mold_png)
     dst3 = cv2.bitwise_or(dst, dst2)
 
     return dst3
@@ -56,6 +57,7 @@ def put_frame(img, mold):
 def emoldurar(imgs_path):
     if os.path.isfile(mold_path) and mold_path.split('.')[-1] == "png":
         mold = cv2.imread(mold_path, cv2.IMREAD_UNCHANGED)
+        mold_bboxs, mold_png = detectar_enquadramento(mold)
     else:
         print("ERRO: verifique se o nome do arquivo está correto e tente novamente.\n")
         return -1
@@ -67,17 +69,47 @@ def emoldurar(imgs_path):
         os.mkdir(result_path)
 
     imgs_list = os.listdir(imgs_path)
-    for img_name in imgs_list:
-        if img_name.split('.')[-1] == "JPG" or img_name.split('.')[-1] == "jpg" or img_name.split('.')[-1] == "png":
+    imgs_list.sort()
+    mold_bboxs = mold_bboxs
+    complete = False
+    result = mold
+    num_frames_put = 0
+    for i, img_name in enumerate(imgs_list):
+        if img_name.split('.')[-1] == "JPG" or img_name.split('.')[-1] == "jpg" or img_name.split('.')[-1] == "jpeg" or img_name.split('.')[-1] == "png":
             print("Carregando: "+imgs_path+'/'+img_name+"\n")
             img = cv2.imread(imgs_path+'/'+img_name)
+            # adiciona um canal a img
+            img = np.dstack([img, np.ones((img.shape[0], img.shape[1]), dtype="uint8") * 255])
+
             if img is not None:
-                result = put_frame(img, mold)
-                cv2.imwrite(result_path+'/'+img_name,result)
+                mold_bbox = mold_bboxs.pop(0)
+                mold_bboxs.append(mold_bbox)
+
+                result = put_frame(img, result, mold_bbox, mold_png)
+                num_frames_put += 1
+                if num_frames_put == len(mold_bboxs):
+                    # cv2.imshow("result", result)
+                    # cv2.waitKey(0)
+                    cv2.imwrite(result_path+'/'+img_name, result)
+                    num_frames_put = 0
+
+                elif i == len(imgs_list)-1:
+                    # Se a ultima imagem não completar a moldura
+                    mold_bbox = mold_bboxs.pop(0)
+                    mold_bboxs.append(mold_bbox)
+                    img = cv2.imread(imgs_path+'/'+imgs_list[0])
+                    # adiciona um canal a img
+                    img = np.dstack([img, np.ones((img.shape[0], img.shape[1]), dtype="uint8") * 255])
+                    result = put_frame(img, result, mold_bbox, mold_png)
+                    # cv2.imshow("result", result)
+                    # cv2.waitKey(0)
+                    cv2.imwrite(result_path+'/'+img_name, result)
+
+
             else:
                 print("ERRO: ao carregar imagem"+imgs_path+'/'+img_name+"\n")
 
 if __name__=="__main__":
-    main()
-
+    mold_path = "/media/samuel/Workspace/auto_framing/molds/MoldPolaroide.png"
+    emoldurar("/media/samuel/Workspace/auto_framing/testes")
 
